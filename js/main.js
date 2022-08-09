ce7c64d4d456971d34aa0f100c132435966843ec
Joan Cortes <js.199911@gmail.com>
2022-08-09 13:24:24 -0500

    Menu lateral 50%

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+/* Close and open menu*/
+const btnMenu = document.querySelector('.btn-n-menu');
+const homeMenu = document.querySelector('.home__menu__side');
+
+btnMenu.addEventListener('click', () => {
+    homeMenu.classList.toggle('home__box--show')
+});
+
 /* Close and open pop-up */
 const btnsClose = document.querySelectorAll('.pop-up .btn-n');
 const popUp = document.querySelector('.pop-up');
@@ -5,5 +13,14 @@ const popUp = document.querySelector('.pop-up');
 btnsClose.forEach((btnClose) => {
     btnClose.addEventListener('click', () => {
         popUp.style.display="none";
+    })
+});
+
+const btnsBoxHome = document.querySelectorAll('.home__box__container__btn');
+const contentBox = document.querySelector('.home__box__container__btn + .home__box__content')
+
+btnsBoxHome.forEach((btnBoxHome) => {
+    btnBoxHome.addEventListener('click', () => {
+        contentBox.classList.toggle('home__box--show')
     })
 });
